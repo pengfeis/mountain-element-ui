7e632dc532378f0339a54241859e570f3a3520d6
pengfeis <supengfei007@gmail.com>
2020-12-28 07:22:45 +0800

    go

--- vue.config.js
+++ vue.config.js
@@ -61,17 +61,16 @@ module.exports = {
       warnings: false,
       errors: true
     }
-    // proxy: {
-    //   // change xxx-api/login => mock/login
-    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
-    //   [process.env.VUE_APP_BASE_API]: {
-    //     target: `http://127.0.0.1:${port}/mock`,
-    //     changeOrigin: true,
-    //     pathRewrite: {
-    //       ['^' + process.env.VUE_APP_BASE_API]: ''
-    //     }
-    //   }
-    // },
+    ,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: 'http://localhost:8080/mt',
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    },
     // after: require('./mock/mock-server.js')
   },
   configureWebpack: {
